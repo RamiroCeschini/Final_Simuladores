5f68c167435e57b56cb12f6cb46cb0c3e29ca2c8
y4m4x <yamilleva177@gmail.com>
2023-11-27 20:01:36 -0300

    InputFix
    Se corrigió el código en los scripts correctos para el funcionamiento de los sliders y se agregaron nuevos sliders para en el futuro configurar su funcionamiento.

--- BombarderoSim/Assets/BranchWork/UI_Info/Scripts/SliderManager.cs
+++ BombarderoSim/Assets/BranchWork/UI_Info/Scripts/SliderManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SliderManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject CylinderTest;
+    public Text XEyeText;
+    public Text YEyeText;
+    public Text ZEyeText;
+    public Vector3 startPosition;
+
+    private void Start()
     {
-        
+        RotationZ(0f);
+        PositionY(0f);
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PositionY(float y)
     {
-        
+        CylinderTest.transform.position = startPosition + new Vector3 (0f, y, 0f);
+    }
+    
+    public void RotationZ(float rotationZ)
+    {
+        CylinderTest.transform.localEulerAngles = new Vector3(-rotationZ, CylinderTest.transform.localEulerAngles.z);
+        ZEyeText.text = rotationZ.ToString() + "°";
+        Debug.Log("rot: " + rotationZ + "euler:" + CylinderTest.transform.localEulerAngles.z);
     }
 }
