3c033914220b54f663de7eba0942f734d983dcc3
TuckoxXL <sebastianbelluccini@gmail.com>
2023-12-10 00:32:23 -0300

    Bomba Fin
    se termino de agregar el funcionamiento junto con las particulas

--- BombarderoSim/Assets/BranchWork/Mec_Bomb/Scripts/DamageDetector.cs
+++ BombarderoSim/Assets/BranchWork/Mec_Bomb/Scripts/DamageDetector.cs
@@ -1,37 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DamageDetector : MonoBehaviour
 {
-    [SerializeField] private float radio;
-    [SerializeField] private float fuerzaExplosion;
+    private float radio;
+    private float daño = 10;
+    private float dañoTotal;
+
+    [Header("Bomba Configuracion")]
+    [SerializeField] private float bomPeso;
     [SerializeField] private GameObject bomba;
     [SerializeField] private Transform bombaTransform;
+    [SerializeField] private Transform explosion;
+    [SerializeField] private GameObject areaDaño;
+
+    [Header("Materiales Configuracion")]
     [SerializeField] private Material original;
     [SerializeField] private Material destruido;
     [SerializeField] private List<MeshRenderer> renderer;
 
+    [Header("Particulas Configuracion")]
+    [SerializeField] private List<ParticleSystem> efectos;
+    [SerializeField] private GameObject particulas;
+
+    [Header("Estadisticas")]
+    [SerializeField] private List<float> dañoEdificios;
+    
+    
+
     public void Explosion()
     {
-        Collider[] objetos = Physics.OverlapSphere(bombaTransform.position, radio);
+        explosion = bombaTransform; 
+        CalculoPeso();
+        Collider[] objetos = Physics.OverlapSphere(explosion.position, radio);
+        EfectoParticulas();
         foreach (Collider colisionador in objetos)
         {
             if (colisionador.TryGetComponent<TMP_Edificio>(out TMP_Edificio edificio)) 
             {
+                float dañoEdificio = Vector3.Distance(explosion.position,colisionador.transform.position);
+                dañoEdificios.Add(dañoEdificio);
                 renderer.Add(colisionador.GetComponent<MeshRenderer>());
-                Rigidbody rb = colisionador.GetComponent<Rigidbody>();
-
-                if (rb != null)
-                {
-                    colisionador.GetComponent<MeshRenderer>().material = destruido;
-                    Invoke("RestaurarColor", 3f);
-                }
+                colisionador.GetComponent<MeshRenderer>().material = destruido;
+                Invoke("RestaurarColor", 3f);
             }
-            
         }
+        SumarDaño();
+        Destroy(bomba);       
+    }
 
-        Destroy(bomba);
+    private void CalculoPeso()
+    {
+        radio = ((bomPeso * 30) / 100);
+        daño = daño + bomPeso;
     }
 
     private void RestaurarColor()
@@ -40,6 +64,29 @@ public class DamageDetector : MonoBehaviour
         {
             rend.material = original;
         }
+        renderer.Clear();
+    }
+
+    private void SumarDaño()
+    {
+        foreach (float daño in dañoEdificios)
+        {
+            dañoTotal += daño;
+            Debug.Log(daño);
+        }
+
+        Debug.Log("DAÑO TOTAL: " + dañoTotal);
+        dañoEdificios.Clear();
+        dañoTotal = 0;
+        
+    }
+
+    private void EfectoParticulas()
+    {
+        GameObject particulasObject = Instantiate(particulas,explosion.position, Quaternion.identity);
+        areaDaño.transform.position = explosion.position;
+        areaDaño.transform.localScale = new Vector3(radio + 2.4f, areaDaño.transform.localScale.y,radio + 2.4f);
+        areaDaño.SetActive(true);
     }
 
     private void OnDrawGizmo()
