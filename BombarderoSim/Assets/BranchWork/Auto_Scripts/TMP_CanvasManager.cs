2318943571ad2cd91804be34206a23934c08b5c2
RamiroCeschini <forchiyceschi@gmail.com>
2023-12-10 20:36:36 -0300

    Camaras pero bien hechas

--- BombarderoSim/Assets/BranchWork/Auto_Scripts/TMP_CanvasManager.cs
+++ BombarderoSim/Assets/BranchWork/Auto_Scripts/TMP_CanvasManager.cs
@@ -5,25 +5,41 @@ using UnityEngine.UI;
 
 public class TMP_CanvasManager : MonoBehaviour
 {
-    [SerializeField] private GameObject SliderCanvas;
-    [SerializeField] private GameObject DataCanvas;
+    [SerializeField] private GameObject Canvas1;
+    [SerializeField] private GameObject Canvas2;
     [SerializeField] private Scrollbar scrollBarResults;
     private bool currentCanvas = false;
-    public void ChangeCanvas()
+    public void ChangeResultCanvas()
     {
         if (!currentCanvas)
         {
-            SliderCanvas.SetActive(false);
-            DataCanvas.SetActive(true);
+            Canvas1.SetActive(false);
+            Canvas2.SetActive(true);
             scrollBarResults.value = 1;
             currentCanvas = true;
         }
         else
         {
-            DataCanvas.SetActive(false);
-            SliderCanvas.SetActive(true);
+            Canvas2.SetActive(false);
+            Canvas1.SetActive(true);
             currentCanvas = false;
         }
     }
+    public void ChangeCanvas()
+    {
+        if (!currentCanvas)
+        {
+            Canvas1.SetActive(false);
+            Canvas2.SetActive(true);
+            currentCanvas = true;
+        }
+        else
+        {
+            Canvas2.SetActive(false);
+            Canvas1.SetActive(true);
+            currentCanvas = false;
+        }
+    }
+
 
 }
