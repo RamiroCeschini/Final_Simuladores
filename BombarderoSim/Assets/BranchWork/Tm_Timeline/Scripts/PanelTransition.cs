473d3c04dba5d06220a7f65bca64192df1eb4fa0
y4m4x <yamilleva177@gmail.com>
2023-12-10 23:31:19 -0300

    Timeline y Animaciones
    Se logró la transición entre los canvas y además se agregó una animación de prueba y un script para configurar la transición de canvas.

--- BombarderoSim/Assets/BranchWork/Tm_Timeline/Scripts/PanelTransition.cs
+++ BombarderoSim/Assets/BranchWork/Tm_Timeline/Scripts/PanelTransition.cs
@@ -2,34 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class PanelTransition : MonoBehaviour
 {
-    public GameObject canvasMenu;
-    public GameObject slidersCanvas;
-    public Button manualButton;
-    public Button automaticButton;
+    public PlayableDirector timeline;
 
-    void Start()
+    private void Start()
     {
-        // Agrega un listener al botón para detectar clics
-        manualButton.onClick.AddListener(OnManualButtonClick);
+        // Desactivar el Panel 2 al inicio
+        timeline.gameObject.SetActive(false);
     }
 
-    void OnManualButtonClick()
+    public void OnButtonClick()
     {
-        // Inicia la transición
-        StartCoroutine(TransitionPanels());
-    }
-
-    IEnumerator TransitionPanels()
-    {
-        // Puedes agregar una animación personalizada o simplemente desactivar/activar los paneles
-        canvasMenu.SetActive(false);
-        slidersCanvas.SetActive(true);
-
-        // Puedes agregar aquí una transición más suave si es necesario
-
-        yield return null;
+        // Activar el Panel 2 y reproducir la transición
+        timeline.gameObject.SetActive(true);
+        timeline.Play();
     }
 }
